dc578ee705cec4a6beef0e7d4a8085fbf7708f27
Ted Bell <tchbell@gmail.com>
2018-01-10 14:21:21 -0500

    one seconds afer player 1 makes a move the computer picks a box

--- main.js
+++ main.js
@@ -1,34 +1,50 @@
 const board = document.querySelector('.board');
+//holds all the possible square values
+let boardContents = [1,2,3,4,5,6,7,8,9];
 //Set up click listener to change cells to X or O depending on player
 board.addEventListener('click',function(e){
   if(e.target.className === 'cell' && e.target.innerHTML === ""){
     //On p1 turn push cell id into myCells so winningCombo function can check for winning combination
     //then change player turn
-    if(p1.myTurn){
-        e.target.innerHTML = 'X';
-        p1.myCells.push(e.target.id);
-        p1.myTurn = false;
-        p2.myTurn = true;
-        winningCombo(p1.myCells);
-    }else if(p2.myTurn){
-        p2.myCells.push(e.target.id);
-        e.target.innerHTML = 'O';
-        p1.myTurn = true;
-        p2.myTurn = false;
-        winningCombo(p2.myCells);
-    }
+    //cell is clicked and thats id is set to square
+    let square = parseInt(e.target.id);
+  //change the cell that was clicked's innerHTML to X
+    e.target.innerHTML = 'X';
+    //take that id and push it into p1myCells
+    p1.myCells.push(e.target.id);
+    //take board contents and remove the index of the id that was selected
+    boardContents.splice(boardContents.indexOf(square), 1);
+    //check if p1 cells contain a winning ocmbo
+    winningCombo(p1.myCells);
+    //after 1 second
+    let computerMove = setTimeout(function(){
+      //pick a random number between 0 and the length of boardcontents
+      let box =  Math.floor(Math.random() * boardContents.length);
+      // change the html of the cell with the id that match the random number picked
+        document.getElementById(boardContents[box]).innerHTML = "O";
+        computer.myCells.push(boardContents[box]);
+        winningCombo(computer.myCells);
+        //then take away that number from board contents
+        boardContents.splice(boardContents.indexOf(boardContents[box]), 1);
+    }, 1000);
   }
 })
 
+
+//clears all cells innerHTML and refills boardContents
 const clearBoard = function(){
   let nodes = board.childNodes;
   for(let i=0; i<nodes.length; i ++){
     nodes[i].innerHTML = "";
   }
+  p1.myCells = [];
+  computer.myCells =[];
+  boardContents = [1,2,3,4,5,6,7,8,9];
 }
 let reset = document.querySelector('.restart');
 reset.addEventListener('click', function(){
   clearBoard();
+
 })
 
 
@@ -36,7 +52,6 @@ reset.addEventListener('click', function(){
 //uses a regex to check if the players myCell array has the proper combination to win the game
 const winningCombo = function check(array){
   let arr = array.join("");
-  console.log(arr);
   switch(true){
     case(/^(?=.*1)(?=.*2)(?=.*3)/.test(arr)):
       alert("Winner, Top Row!");
@@ -78,7 +93,7 @@ const p1 = {
   myTurn : true,
   myCells: []
 }
-const p2 = {
+const computer = {
   myTurn : false,
   myCells: []
 }
